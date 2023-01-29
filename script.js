75bf16a76f53160eeb15bd1339df16925b33c095
dimavamid <113330199+dimavamid@users.noreply.github.com>
2023-01-29 20:25:39 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ function chanse() {
     
     var p1 = document.getElementById('power1').value;
     var p2 = document.getElementById('power2').value;
-    p1 = parseInt (p1);
-    p2 = parseInt (p2);
+    p1 = parseFloat (p1);
+    p2 = parseFloat (p2);
     var ch1 = (p1/(p1+p2))*100;
     var ch2 = 100-ch1;
-    
-
-    document.getElementById('chanse1').innerHTML = ch2;
+    ch1 = ch1.toFixed(2);
+    ch2 = ch2.toFixed(2);
+    document.getElementById('chanse1').innerHTML = ch1;
     document.getElementById('chanse2').innerHTML = ch2;
     
 }
